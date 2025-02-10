3dd4b6fe61331632fceae2f84bb0b0223af4be50
Amelia Johnson <111928731+amelia-online@users.noreply.github.com>
2025-02-10 13:27:10 -0700

    fn defs

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,17 +1,64 @@
 #include "lexer.h"
 
+Token::Token(int start, int end, TokenType type, std::string content) : start(start), end(end), 
+    type(type), content(content) {;}
+
 Lexer::Lexer(std::string input) : input(input), index(0) {;}
 
-std::vector<Token> *Lexer::lex()
+std::vector<Token> Lexer::lex()
 {
-    std::vector<Token> *tokens = new std::vector<Token>();
+    std::vector<Token> tokens;
 
+    while (index < input.length())
+    {
+        char ch = peek();
 
+        if (ch == '\n')
+        {
+            if (tokens.size() == 0 || tokens.back().type != TokenType::NEWLINE)
+            {
+                Token t(index, index+1, TokenType::NEWLINE, "\n");
+                tokens.push_back(t);
+            }
+            index++;
+            continue;
+        }
+
+        if (alphabet.find(ch) != std::string::npos)
+        {
+
+        }
+
+        else if (digits.find(ch) != std::string::npos)
+        {
+
+        }
+    }
 
     return tokens;
 }
 
-char Lexer::now()
+char Lexer::ahead()
+{
+    return offset(1);
+}
+
+char Lexer::offset(size_t offs)
+{
+    if (index + offs < input.length() && index + offs > -1)
+        return input[index + offs];
+    throw new std::exception();
+}
+
+char Lexer::pop()
+{
+    char res = peek();
+    index++;
+    return res;
+}
+
+
+char Lexer::peek()
 {
     return input[index];
 }
--- src/lexer.h
+++ src/lexer.h
@@ -5,18 +5,6 @@
 #include <string>
 #include <vector>
 
-class Lexer
-{
-   std::string input;
-   size_t index;
-   const std::string alphabet = "abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   const std::string digits = "0123456789";
-public:
-    Lexer(std::string);
-    std::vector<Token> *lex();
-    char now();
-};
-
 enum class TokenType
 {
     NEWLINE = 0,
@@ -30,6 +18,7 @@ enum class TokenType
     INTVAL,
     INCLUDE,
     STRING,
+    CSTRING,
     DROP,
     SWAP,
     DUP,
@@ -54,6 +43,7 @@ enum class TokenType
     BIKESHEDDER,
     ASSERT,
     IFSTAR,
+    HERE
 };
 
 class Token
@@ -63,6 +53,28 @@ public:
     int end;
     TokenType type;
     std::string content;
+    Token(int, int, TokenType, std::string);
+};
+
+class Lexer
+{
+   std::string input;
+   size_t index;
+   const std::string alphabet = "abcdefghjiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   const std::string digits = "0123456789";
+public:
+    Lexer(std::string);
+    std::vector<Token> lex();
+    char peek();
+    char pop();
+    char offset(size_t);
+    char ahead();
+    Token lexKeyword();
+    Token lexHex();
+    Token lexFloat();
+    Token lexString();
+    bool isComment();
+    Token lexOperator();
 };
 
 #endif //LEXER_H
